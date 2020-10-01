d06e84b0e6ca27b87d934c670dc044e1ff90ba2f
Slavi Boshnakov <43034168+slaviboshnakov@users.noreply.github.com>
2020-10-01 11:53:47 +0100

    Bosh.io 1.0
    --------
    Cell script
    PlayerMovement
    FoodSpawn ( In progress )
    
    Created Prefabs

---
+++ bosh.io/Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour
+{
+    public GameObject foodSpawnPrefab;
+    public int minX;
+    public int minY;
+    public int maxX;
+    public int maxY;
+
+    public RigidBody2D foodClone;
+
+    void Start()
+    {
+        InvokeRepeating("spawnFood",5f,0.5f);
+    }
+
+    
+
+    public void spawnFood()
+    {
+        var foodClone = Instantiate(foodSpawnPrefab, transform.position, transform.rotation);
+
+    }
+
+}
